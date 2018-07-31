f922a774529a2328013cc8e1f44c10b8db61dbaf
Guy Moyal <guy@moyal.me>
2018-07-31 20:23:24 +0300

    all done besides game over in alef, started anim for bet

--- HW1/HW1/HW1/Form1.Designer.cs
+++ HW1/HW1/HW1/Form1.Designer.cs
@@ -28,15 +28,21 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(364, 372);
+            this.ClientSize = new System.Drawing.Size(485, 465);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.Text = "Form1";
@@ -45,6 +51,8 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- HW1/HW1/HW1/Form1.cs
+++ HW1/HW1/HW1/Form1.cs
@@ -12,10 +12,12 @@ namespace HW1
 {
     public partial class Form1 : Form
     {
-        private int[,] numMtrx = new int[4,4];
+        private int[] numMtrx = new int[15];
+        List<int> rndList = new List<int>();
         private Button[,] btnArray = new Button[4, 4];
         private Random rnd = new Random();
         Button fucker;
+        int rndNumber = 0;
         public Form1()
         {
             InitializeComponent();
@@ -31,77 +33,151 @@ namespace HW1
                     btnArray[i, j].FlatStyle = FlatStyle.Flat;
                     btnArray[i, j].FlatAppearance.BorderColor = Color.Black;
                     btnArray[i, j].FlatAppearance.BorderSize = 1;
-                    while (true)
-                    {
-                        int rndNum = rnd.Next(1, 17);
-                        if (!isNumInArr(rndNum))
-                        {
-                            numMtrx[i, j] = rndNum;
-                            break;
-                        }
-                    }
-                    btnArray[i, j].Text = Convert.ToString((numMtrx[i,j]));
+
                     btnArray[i, j].Width = w;
                     btnArray[i, j].Height = h;
                     btnArray[i, j].Left = 1 + j % 4 * w;
                     btnArray[i, j].Top = i * h;
                     btnArray[i, j].TabIndex = i * 4 + j;
                     btnArray[i, j].Click += new EventHandler(BtnClick);
+                    if (i == 3 && j == 3)
+                    {
+                        btnArray[i, j].Text = "100";
+                    }
+                    else
+                    {
+                        rndNumber = GetRndNum();
+                        btnArray[i, j].Text = Convert.ToString(rndNumber);
+                    }
                     this.Controls.Add(btnArray[i, j]);
                 }
             }
             fucker = btnArray[3, 3];
             HideButton(fucker);
         }
-        private Boolean isNumInArr (int num)
+        private int GetRndNum ()
         {
-            for (int i = 0; i < 4; i++)
+            int retNum = rnd.Next(1,16);
+            while (rndList.Contains(retNum))
             {
-                for (int j = 0; j < 4; j++)
-                {
-                    if (numMtrx[i, j] == num)
-                    {
-                        return true;
-                    }
-                }
+                retNum = rnd.Next(1,16);
             }
-            return false;
+            rndList.Add(retNum);
+            return retNum;
+               
         }
         private void HideButton (Button b)
         {
             b.Visible = false;
             b.Tag = "hidden";
         }
+        private void SwapArray(Button a, Button b)
+        {
+            Button temp;
+            int Ia=0, Ja=0, Ib=0, Jb=0;
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    if (btnArray[i,j].Text == a.Text)
+                    {
+                        Ia = i;
+                        Ja = j;
+                    }
+                    if (btnArray[i,j].Text == b.Text)
+                    {
+                        Ib = i;
+                        Jb = j;
+                    }
+
+                }
+            }
+            temp = a;
+            btnArray[Ia,Ja] = b;
+            btnArray[Ib, Jb] = temp;
+        }
         private void SwapWithHidden(Button b)
         {
+            Button temp = b;
+            int indx = b.TabIndex;
+            Point loc = new Point(b.Top, b.Left); ;
+            //HideButton(b);
+            //fucker.Text = b.Text;
+            //fucker.BackColor = b.BackColor;
+            //fucker.Visible = true;
+            //fucker = b;
+            b.Location = new Point(fucker.Top, fucker.Left);
+            b.TabIndex = fucker.TabIndex;
+            fucker.Location = loc;
+            fucker.TabIndex = indx;
+            SwapArray(b, fucker);
+        }
+        private void SwapWithHiddenAnimate(Button b)
+        {
+            timer1.Start();
+            Button temp = b;
+            int indx = b.TabIndex;
+            Point loc = new Point(b.Top, b.Left); ;
+            b.Location = new Point(fucker.Top, fucker.Left);
+            b.TabIndex = fucker.TabIndex;
+            fucker.Location = loc;
+            fucker.TabIndex = indx;
+            timer1.Stop();
+        }
+        private void AnimateThisShit(Button b, Point dest)
+        {
+            for (int i = 0; i < 40; i++)
+            {
 
-            HideButton(b);
-            fucker.Text = b.Text;
-            fucker.BackColor = b.BackColor;
-            fucker.Visible = true;
-            fucker = b;
-            
-
+            }
         }
         private Boolean CheckWinState()
         {
+            bool sorted = true;
+            int[] tmp = new int[btnArray.Length];
             for (int i = 0; i < 4; i++)
             {
-                for (int j = 1; j < 4; j++)
+                for (int j = 0; j < 4; j++)
                 {
-                    if (Int32.Parse(btnArray[i,j-1].Text) > Int32.Parse(btnArray[i,j].Text))
-                    {
-                        return false;
-                    }
+                    tmp[i * 4 + j] = Int32.Parse(btnArray[i, j].Text);
+                    
                 }
             }
-            return true;
+            for (int i = 1; i < tmp.Length; i++)
+            {
+                if (tmp[i- 1] > tmp[i])
+                {
+                    sorted = false;
+                }
+                if (i == 14)
+                {
+                    if (tmp[14] == 15 && tmp[15] == 14)
+                    {
+                        MessageBox.Show("Game is not winnable");
+                        //this.Close();  pop up the Game Over screen with message not winnable
+                    }
+                }
+                else
+                {
+                    continue;
+                }
+            }
+            if (sorted)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
         private void BtnClick(object sender, EventArgs e)
         {
             if (((Button)sender).TabIndex +1 == fucker.TabIndex || ((Button)sender).TabIndex - 1 == fucker.TabIndex || ((Button)sender).TabIndex + 4 == fucker.TabIndex || ((Button)sender).TabIndex -4 == fucker.TabIndex)
             {
+                //SwapWithHidden((Button)sender);
                 SwapWithHidden((Button)sender);
+
                 if (CheckWinState())
                 {
                     MessageBox.Show("Winner!");
@@ -110,5 +186,10 @@ namespace HW1
 
         }
 
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            MessageBox.Show("tick fucker");
+            Console.WriteLine("tick");
+        }
     }
 }
