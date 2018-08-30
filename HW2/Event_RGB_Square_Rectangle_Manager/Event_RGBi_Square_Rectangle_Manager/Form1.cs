6e4a99d5732f4f2207b2851a86246899c30214e5
Guy Moyal <guy@moyal.me>
2018-08-30 15:14:06 +0300

    added rect min/max

--- HW2/Event_RGB_Square_Rectangle_Manager/Event_RGBi_Square_Rectangle_Manager/Form1.cs
+++ HW2/Event_RGB_Square_Rectangle_Manager/Event_RGBi_Square_Rectangle_Manager/Form1.cs
@@ -14,7 +14,7 @@ namespace Event_RGBi_Square_Rectangle_Manager
     {
         public UserControl1[] arrUC = new UserControl1[2];
         public Control ButtonLabel_MinMax_RectangleSquare_control = null;
-        private List<Control> controlList= new List<Control>();
+        private List<myControl> controlList= new List<myControl>();
         public Form1(string ButtonLabel, string MinMax, string RectangleSquare)
         {
             InitializeComponent();
@@ -39,7 +39,7 @@ namespace Event_RGBi_Square_Rectangle_Manager
             arrUC[0].myEvent += new myDelegate(fromUserControl);
             arrUC[1].myEvent += new myDelegate(fromUserControl);
         }
-        private Color getControlColor()
+        private Color getSelectedColor()
         {
             if (radioButtonRed.Checked) { return Color.FromName("Red"); }
             if (radioButtonGreen.Checked) { return Color.FromName("Green"); }
@@ -47,17 +47,68 @@ namespace Event_RGBi_Square_Rectangle_Manager
             
 
         }
-        // need to go over both user controls, currently only one
+        private ShapeType getSelectedShapeType()
+        {
+            if (Rectangle_Square_label.Text == "Square")
+            {
+                return ShapeType.Square;
+            }
+            else
+            {
+                return ShapeType.Rectangle;
+            }
+        }
+        private ShapeType isControlSquare(Control control)
+        {
+            if (control.Height == control.Width)
+            {
+                return ShapeType.Square;
+            }
+            else
+            {
+                return ShapeType.Rectangle;
+            }
+        }
+
+        //TODO: add try to take care of empty lists
+        //TODO: check that rectangles work
+        private Control getMinControl(List<myControl> controls)
+        {
+            var a = controls.OrderBy(c => c.myHeight*c.myWidth).First();//Max(ctr1 => ctr1.myHeight);
+            return a;
+        }
+        private Control getMaxControl(List<myControl> controls)
+        {
+            var a = controls.OrderByDescending(c => c.myHeight*c.myWidth).First();//Max(ctr1 => ctr1.myHeight);
+            return a;
+        }
+        //TODO: need to go over both user controls, currently only one
         private void fromUserControl(object sender, myEventArgs e)
         {
             foreach (Control control in e.arrControls)
             {
                     //TODO: add to if, fis shape == rect/square?
-                if (control.BackColor == getControlColor() && control.GetType().Name == this.Text)
+                if (control.BackColor == getSelectedColor() && control.GetType().Name == this.Text && isControlSquare(control) == getSelectedShapeType())
                 {
-                    //switch case call and create isMin and isMax function by comparing Size.Width and Size.Height and save the relevant one to temp control to be returned as result
+                    controlList.Add(new myControl(control.BackColor, control.GetType(), control.Width, control.Height));
+                    
                 }
             }
+                    switch (Min_Max_label.Text)
+                    {
+                        case "Min":
+                    Control min = getMinControl(controlList);
+                    ButtonLabel_MinMax_RectangleSquare_control = min;
+                            break;
+                        case "Max":
+                    Control max = getMaxControl(controlList);
+                    ButtonLabel_MinMax_RectangleSquare_control = max;
+
+                    break;
+                        default:
+                            break;
+                    }
+            
         }
 
         private void Form1_FormClosing(object sender, FormClosingEventArgs e)
---
+++ HW2/Event_RGB_Square_Rectangle_Manager/ShapeType.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+public class ShapeType
+{
+	public enum ShapeType
+	{
+        SQUARE, RECTANGLE
+	}
+}
