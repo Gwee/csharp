ab5946d14e033fc0c9cbfc13ebeef4d5eb9bdf03
Guy Moyal <guy@moyal.me>
2018-09-28 16:40:27 +0300

    added hw3

---
+++ HW3/RGB_ButtonsLabels_Transports_UserControls/Extensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.Control;
+
+namespace RGB_ButtonsLabels_Transports_UserControls
+{
+    public static class Extensions
+    {
+        public static Control[] Clone(this ControlCollection c)
+        {
+            var arr_controls = new Control[c.Count];
+            c.CopyTo(arr_controls, 0);
+            return arr_controls;
+        }
+    }
+}
